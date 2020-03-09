38e2975cec7acff6fa80b8a8d998ef38513b5011
lezneklihp <phillip.kenzel@gmail.com>
2020-03-09 22:24:22 +0100

    removed data processing from Dockerfile as this runs into unknown errors

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python process_data.py data/disaster_messages.csv data/disaster_messages.csv data/DisasterResponse.db
 python train_classifier.py data/DisasterResponse.db models/ada_classifier.pkl
 python webapp/run.py ada_classifier.pkl
 flask run --host 0.0.0.0
